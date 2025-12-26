b01999f7b80a69cc6f7eedf00c1486af494dac75
Ashish kumar <ashishgupta845412@gmail.com>
2025-12-26 13:25:45 +0530

    new change

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
+  return res.send("It's working done...🙌");
+
+  
 });
 
 
