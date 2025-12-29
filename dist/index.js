cc286c32bb51c82a18971a56fcb1697f106b5305
Ashish kumar <ashishgupta845412@gmail.com>
2025-12-29 11:43:55 +0530

    changes in index.js

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
-    return res.send("It's working 🙌");
+    return res.send("It's working 🙌....");
 });
 // Create Express route to return a random quote
 const quotes = [
